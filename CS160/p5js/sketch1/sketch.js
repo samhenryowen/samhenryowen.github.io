9086acda5565eaa5069ffcff7a37f8b4bd249106
Sam Owen <46577981+samhenryowen@users.noreply.github.com>
2019-07-27 16:52:47 -0700

    727

---
+++ CS160/p5js/sketch1/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+
+createCanvas(600,480)
+strokeWeight(3);
+ellipseMode(CENTER);
+}
+
+function draw() {
+
+	background(204);
+	fill(255,100,50,255);
+	ellipse(100,100,200);
+	fill(50,255,50,225);
+	ellipse(200,200,200);
+	fill(50,50,255,205);
+	ellipse(300,300,200);
+	fill(255,255,50,185);
+	ellipse(400,400,200);
+	fill(50,255,255,165);
+	rect(400,50,150,150);
+	fill(255,50,255,145);
+	ellipse(0,480,400,400);
+
+}
